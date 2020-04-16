fb19e471cca4c289ce3bf4f0127dc9cf212dcce0
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-04-16 18:35:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,9 +9,9 @@ end
 
 
 def now_serving(queue_line)
-  if queue_line.empty? #.empty used to show that queue_line array is empty
+  if queue_line.empty? #if expression used on conditional basis, .empty used to show that queue_line array is empty
     puts "There is nobody waiting to be served!"
-  else
+  else #else expression used on conditional basis
     puts "Currently serving #{queue_line.first}." #.first method used to show first member of queue_line array.
     queue_line.shift #.shift removes first element of queue_line array
   end
@@ -20,11 +20,11 @@ def now_serving(queue_line)
 end
 
 def line(queue_line)
-  if queue_line.empty? # if condition used , .empty method Returns true when the array is empty
+  if queue_line.empty? # if expresion used on conditionial basis , .empty method used when the array is empty
     puts "The line is currently empty."
-  else
+  else #else expresion used on conditional basis
     current_line = "The line is currently:"
-    queue_line.each.with_index(1) do |person, i|
+    queue_line.each.with_index(1) do |person| #.with_index(1) allows index of queue_line array to start at 1 rather then 0
       current_line << " #{i}. #{person}"
     end
     puts current_line
