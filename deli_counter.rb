82f0131e95fcc8aa19a92e2f0c0c7d27f4dc3445
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-04-16 16:33:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,28 @@
-katz_deli=[]
+queue_line=["Ada","Grace","Kent"]
 
-def take_a_number(katz_deli, name) # take_a_number method,arguments are katz_deli array and name
-  katz_deli << name # name argument, use shovel method to add name argument to end of katz_deli array
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  # uses string interpolation to insert #{name}& #{katz_deli} , uses .length method to output total quantity in katz_deli array
+def take_a_number(queue_line, name) # take_a_number method,arguments are queue_line array and name
+  queue_line << name # name argument, use shovel method to add name argument to end of queue_line array
+  puts "Welcome, #{name}. You are number #{queue_line.length} in line."
+  # uses string interpolation to insert #{name}& #{queue_line} , uses .length method to output total quantity in queue_line array
 end
 
 
-def now_serving(katz_deli) #now_serving method,argument katz_deli array
-  if katz_deli.length == 0 # if condition used, .length method used on katz_deli array, == opperator used to signal 0 to be served
+def now_serving(queue_line) #now_serving method,argument queue_line array
+  if queue_line.length == 0 # if condition used, .length method used on queue_line array, == opperator used to signal 0 to be served
     puts "There is nobody waiting to be served!"
   else #else condition used
-    puts "Currently serving #{katz_deli.shift}." #calling .shift method on array to pull first item from front of katz_deli array
+    puts "Currently serving #{queue_line.shift}." #calling .shift method on array to pull first item from front of queue_line array
   end
 
 end
 
-def line(katz_deli)#line method,argument katz_deli array
-  if katz_deli.length == 0# if condition used, .length method used on katz_deli array, == opperator used to signal 0 to be served
+def line(queue_line)#line method,argument queue_line array
+  if queue_line.length == 0# if condition used, .length method used on queue_line array, == opperator used to signal 0 to be served
     puts "The line is currently empty."
   else
 message="The line is currently:"# message used to return
 
-    katz_deli.each_with_index do |value, index|
+    queue_line.each_with_index do |value, index|
       message += " #{index.to_i+1}. #{value}"
     end
 
