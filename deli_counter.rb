aee1509748ea45c5de327f8a81b4acf0bed4f9aa
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-04-16 17:22:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,9 @@ def take_a_number(queue_line, name)
   # uses string interpolation to insert #{name}& #{queue_line} , uses .length method to output total quantity in queue_line array
 end
 
-
-def now_serving(queue_line) #now_serving method,argument queue_line array
-  if queue_line.length == 0 # if condition used, .length method used on queue_line array, == opperator used to signal 0 to be served
+           # argument queue_line array
+def now_serving(queue_line) #now_serving method
+  if queue_line.length = 0 # if condition used, .length method used on queue_line array, ==  (equality)opperator used to signal 0 to be served
     puts "There is nobody waiting to be served!"
   else #else condition used
     puts "Currently serving #{queue_line.shift}." #calling .shift method on array to pull first item from front of queue_line array
